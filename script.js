9c61be612d8dab82af03a9c896ddd432adbfd2d8
malsha <nimsarani@gmail.com>
2025-04-05 11:05:58 +0530

    GeminiAI API

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const container = document.querySelector(".container");
 const chatsContainer = document.querySelector(".chats-container");
 const promptForm = document.querySelector(".prompt-form");
 const promptInput = promptForm.querySelector(".prompt-input");
@@ -16,10 +17,30 @@ const createMsgElemet = (content, ...classes) => {
     div.innerHTML= content;
     return div;
 }
+//Scroll to the bottom of the container
+const scrollToBottom = () => container.scrollTo({top: container.scrollHeight, behavior: "smooth"});
+
+//Simulate typing effect for bot responses
+const typingEffect=(text, textElement, botMsgDiv) => {
+    textElement.textContent="";
+    const words = text.split(" ");
+    let wordIndex = 0;
+
+    // Set an interval to type each word
+    const typingInterval = setInterval(() => {
+        if(wordIndex<words.length){
+            textElement.textContent += (wordIndex===0? "" : " ")+ words[wordIndex++];
+            botMsgDiv.classList.remove("loading");
+            scrollToBottom();
+        } else {
+            clearInterval(typingInterval);
+        }
+    },40);
+}
 
 //Make the API call and generate the bot's response
-const generateResponse = async (botMsgHTML) => {
-    const textElement = botMsgHTML.querySelector(".message-text")
+const generateResponse = async (botMsgDiv) => {
+    const textElement = botMsgDiv.querySelector(".message-text")
 
     //Add user message to the chat history
     chatHistory.push({
@@ -39,9 +60,9 @@ const generateResponse = async (botMsgHTML) => {
         const data = await response.json();
         if(!response.ok) throw new Error(data.error.message);
 
-        //Process the response text and display it
-        const responseText = data.candidate[0].content.parts[0].text.replace(/\*\*([^*]+)\*\*/g, "$1").trim();
-        textElement.textContent = responseText;
+        //Process the response text and display with typing effect
+        const responseText = data.candidates[0].content.parts[0].text.replace(/\*\*([^*]+)\*\*/g, "$1").trim(); 
+        typingEffect(responseText,textElement, botMsgDiv);
     } catch (error) {
         console.log(error);
     }
@@ -61,13 +82,15 @@ const handleFormSubmit = (e) => {
 
     userMsgDiv.querySelector(".message-text").textContent = userMessage;
     chatsContainer.appendChild(userMsgDiv);
+    scrollToBottom();
 
     setTimeout(() => {
     //Generate bot message HTML and add in the chats container after 600ms
         const botMsgHTML = '<img src="gemini-chatbot-logo.svg" class="avatar"><p class="message-text">Just a sec...</p>';
         const botMsgDiv = createMsgElemet(botMsgHTML, "bot-message", "loading");
         chatsContainer.appendChild(botMsgDiv);
-        generateResponse(botMsgHTML);
+        scrollToBottom();
+        generateResponse(botMsgDiv);
         }, 600);
 }
 
